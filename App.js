d9690fc767a7a859f2963aa1e7c08df59c8917a9
veerthondam <veerthondam@gmail.com>
2024-06-24 16:53:53 +0530

    Component, passing values to component

--- App.js
+++ App.js
@@ -1,48 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+const TitleName = "This is Main Heading";
 
-/* 
-const element = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "span",
-    {
-      class: "parent",
-    },
-    "Hello this is Parent Tagqqqq"
-  ),
-  [
-    React.createElement(
-      "div",
-      {
-        id: "child1",
-      },
-      [
-        React.createElement(
-          "h1",
-          {
-            id: "childern1",
-          },
-          "This is children1"
-        ),
-        React.createElement(
-          "h2",
-          {
-            id: "childern2",
-          },
-          "This is children2"
-        ),
-      ]
-    ),
-  ]
+const Title = () => <h1> {TitleName} </h1>;
+
+const HeaderComponent = () => (
+  <div className="container">
+    <div className="title">
+      <Title />
+    </div>
+  </div>
 );
 
-*/
-const required = true;
-const imgUrl = "https://www.mapsofindia.com/images2/india-map-2019.jpg";
-const element = <img required={true} src={imgUrl} alt="Description of image" />;
-
-console.log(element);
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(element);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
